1a96cd396ef782440ee8a22447c7e0f30ad2095d
jpb <jakub.baranski.33@gmail.com>
2023-12-03 18:47:47 +0000

    Add project files.

---
+++ UTE UWP+/Services/ActivationService.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using UTE_UWP_.Activation;
+using UTE_UWP_.Core.Helpers;
+using UTE_UWP_.Services;
+
+using Windows.ApplicationModel.Activation;
+using Windows.System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace UTE_UWP_.Services
+{
+    // For more information on understanding and extending activation flow see
+    // https://github.com/microsoft/TemplateStudio/blob/main/docs/UWP/activation.md
+    internal class ActivationService
+    {
+        private readonly App _app;
+        private readonly Type _defaultNavItem;
+        private Lazy<UIElement> _shell;
+
+        private object _lastActivationArgs;
+
+        public static readonly KeyboardAccelerator AltLeftKeyboardAccelerator = BuildKeyboardAccelerator(VirtualKey.Left, VirtualKeyModifiers.Menu);
+
+        public static readonly KeyboardAccelerator BackKeyboardAccelerator = BuildKeyboardAccelerator(VirtualKey.GoBack);
+
+        public ActivationService(App app, Type defaultNavItem, Lazy<UIElement> shell = null)
+        {
+            _app = app;
+            _shell = shell;
+            _defaultNavItem = defaultNavItem;
+        }
+
+        public async Task ActivateAsync(object activationArgs)
+        {
+            if (IsInteractive(activationArgs))
+            {
+                // Initialize services that you need before app activation
+                // take into account that the splash screen is shown while this code runs.
+                await InitializeAsync();
+
+                // Do not repeat app initialization when the Window already has content,
+                // just ensure that the window is active
+                if (Window.Current.Content == null)
+                {
+                    // Create a Shell or Frame to act as the navigation context
+                    Window.Current.Content = _shell?.Value ?? new Frame();
+                    NavigationService.NavigationFailed += (sender, e) =>
+                    {
+                        throw e.Exception;
+                    };
+                }
+            }
+
+            // Depending on activationArgs one of ActivationHandlers or DefaultActivationHandler
+            // will navigate to the first page
+            await HandleActivationAsync(activationArgs);
+            _lastActivationArgs = activationArgs;
+
+            if (IsInteractive(activationArgs))
+            {
+                // Ensure the current window is active
+                Window.Current.Activate();
+
+                // Tasks after activation
+                await StartupAsync();
+            }
+        }
+
+        private static KeyboardAccelerator BuildKeyboardAccelerator(VirtualKey key, VirtualKeyModifiers? modifiers = null)
+        {
+            var keyboardAccelerator = new KeyboardAccelerator() { Key = key };
+            if (modifiers.HasValue)
+            {
+                keyboardAccelerator.Modifiers = modifiers.Value;
+            }
+
+            keyboardAccelerator.Invoked += OnKeyboardAcceleratorInvoked;
+            return keyboardAccelerator;
+        }
+
+        private static void OnKeyboardAcceleratorInvoked(KeyboardAccelerator sender, KeyboardAcceleratorInvokedEventArgs args)
+        {
+            var result = NavigationService.GoBack();
+            args.Handled = result;
+        }
+
+        private async Task InitializeAsync()
+        {
+            await ThemeSelectorService.InitializeAsync().ConfigureAwait(false);
+            await WindowManagerService.Current.InitializeAsync();
+        }
+
+        private async Task HandleActivationAsync(object activationArgs)
+        {
+            var activationHandler = GetActivationHandlers()
+                                                .FirstOrDefault(h => h.CanHandle(activationArgs));
+
+            if (activationHandler != null)
+            {
+                await activationHandler.HandleAsync(activationArgs);
+            }
+
+            if (IsInteractive(activationArgs))
+            {
+                var defaultHandler = new DefaultActivationHandler(_defaultNavItem);
+                if (defaultHandler.CanHandle(activationArgs))
+                {
+                    await defaultHandler.HandleAsync(activationArgs);
+                }
+            }
+        }
+
+        private async Task StartupAsync()
+        {
+            // TODO: This is a sample to demonstrate how to add a UserActivity. Please adapt and move this method call to where you consider convenient in your app.
+            await UserActivityService.AddSampleUserActivity();
+            await ThemeSelectorService.SetRequestedThemeAsync();
+            await FirstRunDisplayService.ShowIfAppropriateAsync();
+            await WhatsNewDisplayService.ShowIfAppropriateAsync();
+        }
+
+        private IEnumerable<ActivationHandler> GetActivationHandlers()
+        {
+            yield return Singleton<ToastNotificationsService>.Instance;
+            yield return Singleton<WebToAppLinkActivationHandler>.Instance;
+            yield return Singleton<SchemeActivationHandler>.Instance;
+            yield return Singleton<CommandLineActivationHandler>.Instance;
+        }
+
+        private bool IsInteractive(object args)
+        {
+            return args is IActivatedEventArgs;
+        }
+    }
+}
