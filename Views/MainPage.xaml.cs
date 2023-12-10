80305b9dbeeafb7475e09ede4b5028367c8ad89c
ErrorTek <jakub.baranski.33@gmail.com>
2023-12-10 10:04:49 +0000

    Update version

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -727,7 +727,7 @@ namespace UltraTextEdit_UWP.Views
             ContentDialog aboutDialog = new ContentDialog()
             {
                 Title = "UltraTextEdit UWP",
-                Content = $"Version 1.0 (Build 22000.2295)\n\n© 2021-2023 jpb",
+                Content = $"Version 1.0 (Build 22000.2360)\n\n© 2021-2023 jpb",
                 CloseButtonText = "OK",
                 DefaultButton = ContentDialogButton.Close
             };
