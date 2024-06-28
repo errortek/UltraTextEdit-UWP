c1bc603306470093c0e021f03d4c73c80e83e12d
jpb <jakub.baranski.33@gmail.com>
2024-06-28 19:17:04 +0100

    Add theme switching implementation

---
+++ UTE UWP+/Helpers/SettingsHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Windows.Storage;
+
+namespace UTE_UWP_.Helpers
+{
+    public static class SettingsHelper
+    {
+        public static Object GetSetting(string key)
+        {
+            ApplicationDataContainer LocalSettings = ApplicationData.Current.LocalSettings;
+            return LocalSettings.Values[key];
+        }
+
+        public static Int32 GetSettingInt(string key)
+        {
+            ApplicationDataContainer LocalSettings = ApplicationData.Current.LocalSettings;
+            return (Int32)LocalSettings.Values[key];
+        }
+
+        public static Boolean GetSettingBool(string key)
+        {
+            ApplicationDataContainer LocalSettings = ApplicationData.Current.LocalSettings;
+            return (Boolean)LocalSettings.Values[key];
+        }
+
+        public static String GetSettingString(string key)
+        {
+            ApplicationDataContainer LocalSettings = ApplicationData.Current.LocalSettings;
+            return (String)LocalSettings.Values[key];
+        }
+
+        public static void SetSetting(string key, object value)
+        {
+            ApplicationDataContainer LocalSettings = ApplicationData.Current.LocalSettings;
+            LocalSettings.Values[key] = value;
+        }
+    }
+}
---
+++ UTE UWP+/UI/UI.cs
@@ -0,0 +1,74 @@
+﻿using UTE_UWP_.Helpers;
+using Windows.UI.Xaml;
+using System;
+
+namespace UTE_UWP_.UI
+{
+    public class UI
+    {
+        public UI()
+        {
+
+        }
+
+        public static void ConfigureUIResources()
+        {
+            if (SettingsHelper.GetSettingString("Core.UI") == null)
+            {
+                SettingsHelper.SetSetting("Core.UI", "WinUI");
+            }
+            if (SettingsHelper.GetSettingString("Core.Theme") == "Acrylic Glass")
+            {
+                var res = new ResourceDictionary();
+                res.Source = new Uri("ms-appx:///UI/AcrylicGlassUI.xaml");
+                Application.Current.Resources.MergedDictionaries.Add(res);
+                Application.Current.FocusVisualKind = FocusVisualKind.DottedLine;
+                return;
+            }
+            if (SettingsHelper.GetSettingString("Core.Theme") == "Win32 Light")
+            {
+                var res = new ResourceDictionary();
+                res.Source = new Uri("ms-appx:///UI/Win32 Light.xaml");
+                Application.Current.Resources.MergedDictionaries.Add(res);
+                Application.Current.FocusVisualKind = FocusVisualKind.DottedLine;
+                return;
+            }
+            if (SettingsHelper.GetSettingString("Core.Theme") == "Win32 Dark")
+            {
+                var res = new ResourceDictionary();
+                res.Source = new Uri("ms-appx:///UI/Win32 Dark.xaml");
+                Application.Current.Resources.MergedDictionaries.Add(res);
+                Application.Current.FocusVisualKind = FocusVisualKind.DottedLine;
+                return;
+            }
+            if (SettingsHelper.GetSettingString("Core.UI") == "WinUI")
+            {
+                var res = new ResourceDictionary();
+                res.Source = new Uri("ms-appx:///UI/WinUI.xaml");
+                Application.Current.FocusVisualKind = FocusVisualKind.HighVisibility;
+                Application.Current.Resources.MergedDictionaries.Add(res);
+            }
+            if (SettingsHelper.GetSettingString("Core.UI") == "CrimsonUI")
+            {
+                var res = new ResourceDictionary();
+                res.Source = new Uri("ms-appx:///UI/CrimsonUI.xaml");
+                Application.Current.FocusVisualKind = FocusVisualKind.Reveal;
+                Application.Current.Resources.MergedDictionaries.Add(res);
+            }
+            if (SettingsHelper.GetSettingString("Core.UI") == "10 Light")
+            {
+                var res = new ResourceDictionary();
+                res.Source = new Uri("ms-appx:///UI/10 Light.xaml");
+                Application.Current.FocusVisualKind = FocusVisualKind.HighVisibility;
+                Application.Current.Resources.MergedDictionaries.Add(res);
+            }
+            if (SettingsHelper.GetSettingString("Core.UI") == "10 Dark")
+            {
+                var res = new ResourceDictionary();
+                res.Source = new Uri("ms-appx:///UI/10 Dark.xaml");
+                Application.Current.FocusVisualKind = FocusVisualKind.HighVisibility;
+                Application.Current.Resources.MergedDictionaries.Add(res);
+            }
+        }
+    }
+}
