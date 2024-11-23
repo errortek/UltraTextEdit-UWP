0e1d59b24b5e0e94f5ad0a7e4c26215aee900830
ErrorTek <jakub.baranski.33@gmail.com>
2024-11-23 10:54:23 -0800

    Implement working Markdown changelog

---
+++ UTE UWP+/Assets/Changelogs/Latest.md
@@ -0,0 +1,6 @@
+- Introduce a Home Menu
+- Introduce a new, revitalized, UTE Update system
+- Introduce a Markdown-based changelog renderer
+- Reintroduce table support
+- Reintroduce the ability to open the What's new and Changelog dialogs from the Ribbon (through a VelocityID)
+- Localize any remaining resources
--- UTE UWP+/Views/WhatsNewDialog.xaml.cs
+++ UTE UWP+/Views/WhatsNewDialog.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.IO;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -7,11 +8,16 @@ namespace UTE_UWP_.Views
 {
     public sealed partial class WhatsNewDialog : ContentDialog
     {
+
+        private string changelog;
+        private Uri changelogUri = new Uri("ms-appx:///Assets/Changelogs/Latest.md");
+
         public WhatsNewDialog()
         {
             // TODO: Update the contents of this dialog every time you release a new version of the app
             RequestedTheme = (Window.Current.Content as FrameworkElement).RequestedTheme;
             InitializeComponent();
+            changelog = File.ReadAllText("Assets/Changelogs/Latest.md");
         }
     }
 }
