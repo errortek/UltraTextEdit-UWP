5f0ae681631f5b46e46b1a8f63b00b4c792f7436
jpb <jakub.baranski.33@gmail.com>
2024-12-04 22:45:07 +0000

    Implement checking for updates

--- UTE UWP+/Views/UTEUpdate.xaml.cs
+++ UTE UWP+/Views/UTEUpdate.xaml.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -27,10 +30,27 @@ namespace UTE_UWP_.Views
             this.InitializeComponent();
         }
 
-        private void CheckForUpdates(object sender, RoutedEventArgs e)
+        private async void CheckForUpdates(object sender, RoutedEventArgs e)
         {
             updatecheckProgress.Visibility = Visibility.Visible;
             checkforupdateText.Text = "Checking for updates...";
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead("");
+            StreamReader reader = new StreamReader(stream);
+            var newVersion = new Version(await reader.ReadToEndAsync());
+            Package package = Package.Current;
+            PackageVersion packageVersion = package.Id.Version;
+            var currentVersion = new Version(string.Format("{0}.{1}.{2}.{3}", packageVersion.Major, packageVersion.Minor, packageVersion.Build, packageVersion.Revision));
+
+            //compare package versions
+            if (newVersion.CompareTo(currentVersion) > 0)
+            {
+                checkforupdateText.Text = "An update is available";
+            }
+            else
+            {
+                checkforupdateText.Text = "No updates available";
+            }
         }
     }
 }
